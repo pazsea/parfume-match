a6a9f17f380d979f43292e4de36d07b82a6d8491
glindberg <gustav.lindberg1@gmail.com>
2019-04-03 13:36:53 +0200

    Created quiz, added images.

--- src/components/Quiz/index.js
+++ src/components/Quiz/index.js
@@ -1,17 +1,9 @@
 import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Switch,
-  Link,
-} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { compose } from 'recompose';
-
 import { withAuthorization } from '../Session';
 import { withFirebase } from '../Firebase';
-// import { QUESTIONONE } from '../../constants/routes';
 import * as ROUTES from '../../constants/routes';
-import { QuestionOneImageOne } from '../Quiz/styles';
 import quizStep1Everything from '../../images/quizStep1Everything.jpg';
 import quizStep1Man from '../../images/quizStep1Man.jpg';
 import quizStep1Unisex from '../../images/quizStep1Unisex.jpg';
@@ -19,7 +11,22 @@ import quizStep1Woman from '../../images/quizStep1Woman.jpg';
 import quizStep2ILikeParfume from '../../images/quizStep2ILikeParfume.jpg';
 import quizStep2ImThinkingOf from '../../images/quizStep2ImThinkingOf.jpg';
 import quizStep2GiveMeSomething from '../../images/quizStep2GiveMeSomething.jpg';
-import { quizStep3AtGym } from '../../images/index.js';
+import quizStep3AtGym from '../../images/quizStep3AtGym.jpg';
+import quizStep3AtHome from '../../images/quizStep3AtHome.jpg';
+import quizStep3AtWork from '../../images/quizStep3AtWork.jpg';
+import quizStep3Club from '../../images/quizStep3Club.jpg';
+import quizStep3OnDate from '../../images/quizStep3OnDate.jpg';
+import quizStep3Vernissage from '../../images/quizStep3Vernissage.jpg';
+import quizStep4Coffee from '../../images/quizStep4Coffee.jpg';
+import quizStep4Water from '../../images/quizStep4Water.jpg';
+import quizStep4Cocktail from '../../images/quizStep4Cocktail.jpg';
+import quizStep4Tea from '../../images/quizStep4Tea.jpg';
+import quizStep4Wine from '../../images/quizStep4Wine.jpg';
+import quizStep4Beer from '../../images/quizStep4Beer.jpg';
+import quizStep5Confident from '../../images/quizStep5Confident.jpg';
+import quizStep5Elegant from '../../images/quizStep5Elegant.jpg';
+import quizStep5Unique from '../../images/quizStep5Unique.jpg';
+import quizStep5Sensual from '../../images/quizStep5Sensual.jpg';
 
 // Quiz page är huvudkomponenten som har tre states för slutföring.
 // Quiz page har tre komponenter renderas utifall state är tillgängligt.
@@ -51,11 +58,11 @@ export class QuestionOne extends Component {
         <h1>Vilken parfymtyp är du ute efter?</h1>
         <p>Alternativ: För kvinnor, unisex, för män, allt</p>
 
-        <img src={quizStep1Everything} />
-        <img src={quizStep1Man} />
-        <img src={quizStep1Unisex} />
-        <img src={quizStep1Woman} />
-        <br />
+        <img src={quizStep1Everything} alt="" />
+        <img src={quizStep1Man} alt="" />
+        <img src={quizStep1Unisex} alt="" />
+        <img src={quizStep1Woman} alt="" />
+
         <Link to={ROUTES.QUESTIONTWO}>NÄSTA FRÅGA</Link>
       </div>
     );
@@ -71,9 +78,9 @@ export class QuestionTwo extends Component {
           Alternativ: Jag gillar parfym men inte mer, Jag tänker på
           topp- och basnoter, ge mig något att bita i!
         </p>
-        <img src={quizStep2ILikeParfume} />
-        <img src={quizStep2ImThinkingOf} />
-        <img src={quizStep2GiveMeSomething} />
+        <img src={quizStep2ILikeParfume} alt="" />
+        <img src={quizStep2ImThinkingOf} alt="" />
+        <img src={quizStep2GiveMeSomething} alt="" />
 
         <Link to={ROUTES.QUESTIONTHREE}>NÄSTA FRÅGA</Link>
       </div>
@@ -85,13 +92,18 @@ export class QuestionThree extends Component {
   render() {
     return (
       <div>
-        <h1>Nr vill du dofta extra härligt?</h1>
+        <h1>När vill du dofta extra härligt?</h1>
         <p>
           På jobbet, hemma, på klubben, på gymmet, på nästa dejt, på
           vernissage
         </p>
 
-        <img src={quizStep3AtGym} />
+        <img src={quizStep3AtGym} alt="" />
+        <img src={quizStep3AtHome} alt="" />
+        <img src={quizStep3AtWork} alt="" />
+        <img src={quizStep3Club} alt="" />
+        <img src={quizStep3OnDate} alt="" />
+        <img src={quizStep3Vernissage} alt="" />
 
         <Link to={ROUTES.QUESTIONFOUR}>NÄSTA FRÅGA</Link>
       </div>
@@ -106,6 +118,13 @@ export class QuestionFour extends Component {
         <h1>Vilka är dina favoritdrycker?</h1>
         <p>Alternativ: Kaffe, vatten, cocktails, te, vin, öl</p>
 
+        <img src={quizStep4Coffee} alt="" />
+        <img src={quizStep4Water} alt="" />
+        <img src={quizStep4Cocktail} alt="" />
+        <img src={quizStep4Tea} alt="" />
+        <img src={quizStep4Wine} alt="" />
+        <img src={quizStep4Beer} alt="" />
+
         <Link to={ROUTES.QUESTIONFIVE}>NÄSTA FRÅGA</Link>
       </div>
     );
@@ -118,6 +137,12 @@ export class QuestionFive extends Component {
       <div>
         <h1>Hur vill du känna dig i din parfym?</h1>
         <p>Alternativ: Självsäker, elegant, unik, sensuell</p>
+
+        <img src={quizStep5Confident} alt="" />
+        <img src={quizStep5Elegant} alt="" />
+        <img src={quizStep5Unique} alt="" />
+        <img src={quizStep5Sensual} alt="" />
+
         <Link to={ROUTES.QUESTIONSIX}>NÄSTA FRÅGA</Link>
       </div>
     );
@@ -129,11 +154,12 @@ export class QuestionSix extends Component {
     return (
       <div>
         <h1>Vilka är dina favoritparfymer idag?</h1>
-        <p>
-          Placeholdertext: "Skriv namnen på parfymer du tycker om och
+        <input
+          placeholder="Skriv namnen på parfymer du tycker om och
           tryck return efter varje. I vissa fall ger vi förslag och då
           är det bra om du väljer dem i listan för bättre matchning"
-        </p>
+        />
+
         <Link to={ROUTES.WARDROBE}>Visa min Sniph-kollektion</Link>
       </div>
     );

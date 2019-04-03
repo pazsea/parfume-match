66b46fde0508644b1f9da7e28100deae3a74c82c
glindberg <gustav.lindberg1@gmail.com>
2019-04-03 13:48:07 +0200

    Resolved merge conflict.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { compose } from 'recompose';
 
 import Navigation from '../Navigation';
 import LandingPage from '../Landing';
@@ -23,31 +25,96 @@ import WardrobePage from '../Wardrobe';
 import * as ROUTES from '../../constants/routes';
 import { withAuthentication } from '../Session';
 
-const App = () => (
-  <Router>
-    <div>
-      <Navigation />
+class App extends Component {
+  state = {
+    innerHeight: window.innerHeight,
+    innerWidth: window.innerWidth,
+  };
+  componentDidMount() {
+    // const { innerHeight, innerWidth } = this.state;
+    this.handleResize();
+    window.addEventListener('resize', () => {
+      this.setState({
+        innerHeight: window.innerHeight,
+        innerWidth: window.innerWidth,
+      });
+      this.handleResize();
+    });
+  }
 
-      <Route exact path={ROUTES.LANDING} component={LandingPage} />
-      <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
-      <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-      <Route
-        path={ROUTES.PASSWORD_FORGET}
-        component={PasswordForgetPage}
-      />
-      <Route path={ROUTES.HOME} component={HomePage} />
-      <Route path={ROUTES.ACCOUNT} component={AccountPage} />
-      <Route path={ROUTES.ADMIN} component={AdminPage} />
-      <Route path={ROUTES.QUIZ} component={QuizPage} />
-      <Route path={ROUTES.QUESTIONONE} component={QuestionOne} />
-      <Route path={ROUTES.QUESTIONTWO} component={QuestionTwo} />
-      <Route path={ROUTES.QUESTIONTHREE} component={QuestionThree} />
-      <Route path={ROUTES.QUESTIONFOUR} component={QuestionFour} />
-      <Route path={ROUTES.QUESTIONFIVE} component={QuestionFive} />
-      <Route path={ROUTES.QUESTIONSIX} component={QuestionSix} />
-      <Route path={ROUTES.WARDROBE} component={WardrobePage} />
-    </div>
-  </Router>
-);
+  componentWillUnmount() {
+    console.log('ooout');
+  }
 
-export default withAuthentication(App);
+  handleResize() {
+    const { innerHeight, innerWidth } = this.state;
+    if (
+      innerWidth <= 1024 &&
+      innerWidth >= 768 &&
+      innerHeight <= 1366 &&
+      innerHeight >= 1024
+    ) {
+      this.props.setMediumSize();
+    } else if (innerWidth < 768 && innerHeight < 1024) {
+      this.props.setSmallSize();
+    } else {
+      this.props.setBigSize();
+    }
+  }
+
+  render() {
+    return (
+      <Router>
+        <div>
+          <Navigation />
+
+          <Route
+            exact
+            path={ROUTES.LANDING}
+            component={LandingPage}
+          />
+          <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+          <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+          <Route
+            path={ROUTES.PASSWORD_FORGET}
+            component={PasswordForgetPage}
+          />
+          <Route path={ROUTES.HOME} component={HomePage} />
+          <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+          <Route path={ROUTES.ADMIN} component={AdminPage} />
+          <Route path={ROUTES.QUIZ} component={QuizPage} />
+          <Route path={ROUTES.QUESTIONONE} component={QuestionOne} />
+          <Route path={ROUTES.QUESTIONTWO} component={QuestionTwo} />
+          <Route
+            path={ROUTES.QUESTIONTHREE}
+            component={QuestionThree}
+          />
+          <Route
+            path={ROUTES.QUESTIONFOUR}
+            component={QuestionFour}
+          />
+          <Route
+            path={ROUTES.QUESTIONFIVE}
+            component={QuestionFive}
+          />
+          <Route path={ROUTES.QUESTIONSIX} component={QuestionSix} />
+          <Route path={ROUTES.WARDROBE} component={WardrobePage} />
+        </div>
+      </Router>
+    );
+  }
+}
+
+const mapDispatchToProps = dispatch => ({
+  setBigSize: () => dispatch({ type: 'BIG_SIZE' }),
+  setMediumSize: () => dispatch({ type: 'MEDIUM_SIZE' }),
+  setSmallSize: () => dispatch({ type: 'SMALL_SIZE' }),
+});
+
+export default compose(
+  withAuthentication,
+  connect(
+    null,
+    mapDispatchToProps,
+  ),
+)(App);

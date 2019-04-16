7e06006260ebc7ace980c9c8bbcccbdd6bfc535d
paz198787 <patrick.sjoberg@student.kyh.se>
2019-04-16 11:55:20 +0200

    Plan B

--- src/components/Wardrobe/styles.js
+++ src/components/Wardrobe/styles.js
@@ -7,9 +7,18 @@ export const QuizTitle = styled.div`
   font-weight: 900;
   font-size: 1em;
 `;
-export const QuizSubTitle = styled.div`
-  text-align: center;
-  letter-spacing: 0;
-  font-weight: 300;
-  font-size: 1em;
+
+export const Wrapper = styled.div`
+  width: 100%;
+  border: 1px solid black;
+`;
+
+export const ParfumeDiv = styled.div`
+  border: 1px solid green;
+  min-height: 25rem;
+  margin: 0 4em 1em 5em;
+  display: flex;
+  div {
+    flex: 2;
+  }
 `;

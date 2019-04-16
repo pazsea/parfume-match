631a251968471d178f9539e6ff493a5eae712306
paz198787 <patrick.sjoberg@student.kyh.se>
2019-04-16 11:55:33 +0200

    Plan B

--- src/components/Wardrobe/index.js
+++ src/components/Wardrobe/index.js
@@ -1,21 +1,107 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Truncate from 'react-truncate';
 import { compose } from 'recompose';
-import '../../../node_modules/react-image-gallery/styles/css/image-gallery.css';
+
 import { withAuthorization } from '../Session';
 import { withFirebase } from '../Firebase';
-import { QuizTitle, QuizSubTitle } from './styles';
+import {
+  QuizTitle,
+  Wrapper,
+  ParfumeDiv,
+  ImageDiv,
+  InnerImageDiv,
+  RatingForm,
+} from './styles';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+
+// import StarRatingComponent from 'react-star-rating-component';
+
+import '../../../node_modules/react-image-gallery/styles/css/image-gallery.css';
+import parfume1 from '../../images/parfume1.jpg';
+import 'react-tabs/style/react-tabs.css';
 
 class WardrobePage extends Component {
+  state = {
+    rating: 1,
+    isTruncated: false,
+  };
+
+  toggleTruncate = () => {
+    this.setState(prevState => ({
+      isTruncated: !prevState.isTruncated,
+    }));
+  };
+
   render() {
+    const { isTruncated } = this.state;
     return (
       <div>
         <QuizTitle>
           <h1>Wardrobe</h1>
         </QuizTitle>
-        <QuizSubTitle>
-          <p>This is the Wardrobe</p>
-        </QuizSubTitle>
+        <Wrapper>
+          <ImageDiv>
+            <img src={parfume1} />
+          </ImageDiv>
+
+          <ParfumeDiv>
+            <Tabs>
+              <TabList>
+                <Tab>Parfume name</Tab>
+                <Tab>My rating</Tab>
+              </TabList>
+
+              <TabPanel>
+                <div>
+                  <h2>Parfym 1</h2>
+                  <Truncate
+                    lines={isTruncated ? 0 : 15}
+                    ellipsis={
+                      <span>
+                        ...{' '}
+                        <a onClick={this.toggleTruncate}>
+                          {' '}
+                          Read more
+                        </a>
+                      </span>
+                    }
+                  >
+                    The girls looked like tall, exotic grazing
+                    animals, swaying gracefully and unconsciously with
+                    the movement of the train, their high heels like
+                    polished hooves against the gray metal of the
+                    bright void beyond the chain link. Her cheekbones
+                    flaring scarlet as Wizard’s Castle burned,
+                    forehead drenched with azure when Munich fell to
+                    the Tank War, mouth touched with hot gold as a
+                    gliding cursor struck sparks from the banks of
+                    every computer in the puppet place had been a
+                    subunit of Freeside’s security system. The girls
+                    looked like tall, exotic grazing animals, swaying
+                    gracefully and unconsciously with the surgery, he
+                    {/* <a onClick={this.toggleTruncate}> Read less</a> */}
+                  </Truncate>
+                </div>
+              </TabPanel>
+              <TabPanel>
+                <h2>Rating</h2>
+                <RatingForm onSubmit={this.handleSubmit}>
+                  <label>
+                    My feedback:
+                    <br />
+                    <input
+                      type="textarea"
+                      value={this.state.value}
+                      onChange={this.handleChange}
+                    />
+                  </label>
+                  <input type="submit" value="Submit" />
+                </RatingForm>
+              </TabPanel>
+            </Tabs>
+          </ParfumeDiv>
+        </Wrapper>
       </div>
     );
   }
--- src/components/Wardrobe/styles.js
+++ src/components/Wardrobe/styles.js
@@ -11,14 +11,36 @@ export const QuizTitle = styled.div`
 export const Wrapper = styled.div`
   width: 100%;
   border: 1px solid black;
+  display: flex;
+  justify-content: center;
+  align-content: center;
 `;
 
 export const ParfumeDiv = styled.div`
+  margin: 1em;
+  flex: 1;
   border: 1px solid green;
   min-height: 25rem;
-  margin: 0 4em 1em 5em;
   display: flex;
-  div {
-    flex: 2;
+`;
+
+export const ImageDiv = styled.div`
+  margin: 1em;
+  flex: 1;
+  border: 1px solid red;
+  display: flex;
+  display: flex;
+  justify-content: center;
+
+  img {
+    align-self: center;
   }
+`;
+
+export const RatingForm = styled.form`
+  width: 100%;
+  border: 1px solid yellow;
+  display: flex;
+  flex-direction: column;
+  height: 100%;
 `;

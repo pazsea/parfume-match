75b0d29152a23c3c32c3704d371b10ba665f0036
Patrick Sjöberg <43534461+pazsea@users.noreply.github.com>
2019-04-17 17:38:07 +0200

    Created wardrobe component with standard styling (#14)

--- src/components/Wardrobe/index.js
+++ src/components/Wardrobe/index.js
@@ -1,24 +1,192 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
+import Truncate from 'react-truncate';
 import { compose } from 'recompose';
-import '../../../node_modules/react-image-gallery/styles/css/image-gallery.css';
+
 import { withAuthorization } from '../Session';
 import { withFirebase } from '../Firebase';
-import { QuizTitle, QuizSubTitle } from './styles';
+import * as s from './styles';
+import StarRatingComponent from 'react-star-rating-component';
+import parfume1 from '../../images/parfume1.jpg';
 
 class WardrobePage extends Component {
+  state = {
+    isTruncated: false,
+    tabOpen: 'descriptionTab1',
+    rating: 1,
+  };
+
+  onStarClick(nextValue, prevValue, name) {
+    this.setState({ rating: nextValue });
+  }
+
+  toggleTab = e => {
+    const tabValue = e.target.value;
+    this.setState({
+      tabOpen: tabValue,
+    });
+  };
+
+  toggleTruncate = () => {
+    this.setState(prevState => ({
+      isTruncated: !prevState.isTruncated,
+    }));
+  };
+
   render() {
+    const { tabOpen, isTruncated, value, rating } = this.state;
+    const nombre = 1;
     return (
-      <div>
-        <QuizTitle>
+      <Fragment>
+        <s.QuizTitle>
           <h1>Wardrobe</h1>
-        </QuizTitle>
-        <QuizSubTitle>
-          <p>This is the Wardrobe</p>
-        </QuizSubTitle>
-      </div>
+        </s.QuizTitle>
+        <s.Wrapper>
+          <s.ImageDiv>
+            <img src={parfume1} />
+          </s.ImageDiv>
+          <s.ParfumeDiv>
+            <s.ButtonDiv tabOpen={tabOpen}>
+              <button
+                value={'descriptionTab' + nombre}
+                onClick={e => this.toggleTab(e)}
+              >
+                Description
+              </button>
+              <button
+                value={'ratingTab' + nombre}
+                onClick={e => this.toggleTab(e)}
+              >
+                My Rating
+              </button>
+            </s.ButtonDiv>
+            <s.HeaderDiv>Parfym</s.HeaderDiv>
+            <s.StarsDiv>
+              <StarRatingComponent
+                name="rate1"
+                starCount={5}
+                value={rating}
+                onStarClick={this.onStarClick.bind(this)}
+              />
+            </s.StarsDiv>
+            {tabOpen === 'descriptionTab' + nombre ||
+            tabOpen === '' ? (
+              <DescriptionWrapper
+                rating={rating}
+                isTruncated={isTruncated}
+                toggleTruncate={this.toggleTruncate}
+                tabOpen={tabOpen}
+              />
+            ) : tabOpen === 'ratingTab' + nombre ? (
+              <RatingWrapper
+                handleSubmit={this.handleSubmit}
+                handleChange={this.handleChange}
+                value={value}
+                starClick={this.onStarClick}
+                tabOpen={tabOpen}
+              />
+            ) : null}
+          </s.ParfumeDiv>
+        </s.Wrapper>
+        <s.Wrapper>
+          <s.ImageDiv>
+            <img src={parfume1} />
+          </s.ImageDiv>
+          <s.ParfumeDiv>
+            <s.ButtonDiv tabOpen={tabOpen}>
+              <button
+                value={'descriptionTab' + nombre}
+                onClick={e => this.toggleTab(e)}
+              >
+                Description
+              </button>
+              <button
+                value={'ratingTab' + nombre}
+                onClick={e => this.toggleTab(e)}
+              >
+                My Rating
+              </button>
+            </s.ButtonDiv>
+            <s.HeaderDiv>Parfym</s.HeaderDiv>
+            <s.StarsDiv>
+              <StarRatingComponent
+                name="rate1"
+                starCount={5}
+                value={rating}
+                onStarClick={this.onStarClick.bind(this)}
+              />
+            </s.StarsDiv>
+            {tabOpen === 'descriptionTab' + nombre ||
+            tabOpen === '' ? (
+              <DescriptionWrapper
+                rating={rating}
+                isTruncated={isTruncated}
+                toggleTruncate={this.toggleTruncate}
+                tabOpen={tabOpen}
+              />
+            ) : tabOpen === 'ratingTab' + nombre ? (
+              <RatingWrapper
+                handleSubmit={this.handleSubmit}
+                handleChange={this.handleChange}
+                value={value}
+                starClick={this.onStarClick}
+                tabOpen={tabOpen}
+              />
+            ) : null}
+          </s.ParfumeDiv>
+        </s.Wrapper>
+      </Fragment>
     );
   }
+}
+
+function DescriptionWrapper({ toggleTruncate, isTruncated }) {
+  return (
+    <Fragment>
+      <s.DescriptionDiv>
+        <Truncate
+          lines={isTruncated ? 0 : 9}
+          ellipsis={
+            <span>
+              ... <a onClick={toggleTruncate}> Read more</a>
+            </span>
+          }
+        >
+          2017 års stora lansering från parfymprofilen Pierre
+          Guillaumes doftkollektion "Huitième Art" är en makalös
+          splash av fruktiga ackord, träiga noter och mustiga kryddor.
+          2017 års stora lansering från parfymprofilen Pierre
+          Guillaumes doftkollektion "Huitième Art" är en makalös
+          splash av fruktiga ackord, träiga noter och mustiga kryddor.
+          <br />
+          Att du inom kort känner att du bara måste duscha dig i
+          Aqaysos är inte otänkbart. Blir detta doften som förändrar
+          ditt liv?
+        </Truncate>
+      </s.DescriptionDiv>
+    </Fragment>
+  );
+}
+
+function RatingWrapper({ handleSubmit, handleChange, value }) {
+  return (
+    <Fragment>
+      <s.RatingForm onSubmit={handleSubmit}>
+        <s.RatingBox
+          type="textarea"
+          className="ratingBox"
+          value={value}
+          onChange={handleChange}
+        />
+
+        <s.RatingButton
+          className="ratingButton"
+          type="submit"
+          value="Submit"
+        />
+      </s.RatingForm>
+    </Fragment>
+  );
 }
 
 const mapStateToProps = state => ({
--- src/components/Wardrobe/styles.js
+++ src/components/Wardrobe/styles.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import styled from 'styled-components';
 
 export const QuizTitle = styled.div`
@@ -7,9 +6,122 @@ export const QuizTitle = styled.div`
   font-weight: 900;
   font-size: 1em;
 `;
-export const QuizSubTitle = styled.div`
-  text-align: center;
-  letter-spacing: 0;
-  font-weight: 300;
+
+export const Wrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  margin: 0.8em 0;
+`;
+
+// ONE WHOLE CARD
+export const ImageDiv = styled.div`
+  flex-basis: 40%;
+  display: flex;
+  justify-content: center;
+
+  img {
+    align-self: center;
+    width: 50%;
+    /* height: auto; */
+  }
+`;
+
+export const ParfumeDiv = styled.div`
+  flex-basis: 40%;
+  display: flex;
+  flex-direction: column;
+  box-shadow: 4px 2px 2px black;
+  border: 1px solid #ccc;
+`;
+
+export const ButtonDiv = styled.div`
+  display: flex;
+  flex: 2;
+  flex-direction: row;
+  overflow: hidden;
+  border-bottom: 1px solid #ccc;
+  background-color: white;
+  align-items: center;
+
+  button {
+    cursor: pointer;
+    border: none;
+    font-size: 1.2em;
+    font-weight: 700;
+    background: none;
+    height: fit-content;
+    padding: 0;
+    margin-left: 0.4em;
+  }
+  button:nth-child(1) {
+    border-bottom: ${props =>
+      props.tabOpen === 'descriptionTab1' ? '1px solid black' : null};
+  }
+
+  button:nth-child(2) {
+    margin-left: 0.9em;
+
+    border-bottom: ${props =>
+      props.tabOpen === 'ratingTab1' ? '1px solid black' : null};
+  }
+  button:hover {
+    background-color: #ddd;
+  }
+`;
+
+export const StarsDiv = styled.div`
+  flex: 1;
+  font-size: 2em;
+  margin-left: 0.4em;
+`;
+export const HeaderDiv = styled.div`
+  flex: 1;
+  margin-left: 0.4em;
+  font-family: 'Montserrat', sans-serif;
+  font-weight: 200;
+  font-size: 1.4em;
+  padding: 0.1em 0;
+`;
+
+//DESCRIPTION TAB
+
+export const DescriptionDiv = styled.div`
+  flex: 8;
+  margin-left: 0.4em;
   font-size: 1em;
+  font-family: 'Roboto', sans-serif;
+  font-weight: 100;
+`;
+
+//MY RATING TAB
+
+export const RatingForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  flex: 8;
+  label {
+    font-size: 1.5em;
+  }
+`;
+
+export const RatingBox = styled.textarea`
+  flex: 5;
+  margin: 0 0.4em 0.4em 0.4em;
+`;
+
+export const RatingButton = styled.input`
+  flex: 1;
+  color: #fff;
+  background: #000;
+  margin: 0 0.4em 0.4em 0.4em;
+  border: 2px solid #000;
+  text-transform: uppercase;
+
+  :hover {
+    color: #000;
+    background: white;
+    border: 2px solid black;
+    cursor: pointer;
+  }
 `;

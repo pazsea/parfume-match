416bf534994a7589edd33723cfe2fd6e936b2bf9
paz198787 <patrick.sjoberg@student.kyh.se>
2019-04-17 07:08:11 +0200

    better css

--- src/components/Wardrobe/index.js
+++ src/components/Wardrobe/index.js
@@ -1,21 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import Truncate from 'react-truncate';
 import { compose } from 'recompose';
 
 import { withAuthorization } from '../Session';
 import { withFirebase } from '../Firebase';
-import {
-  QuizTitle,
-  Wrapper,
-  ParfumeDiv,
-  ImageDiv,
-  InnerImageDiv,
-  RatingForm,
-} from './styles';
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-
-// import StarRatingComponent from 'react-star-rating-component';
+import * as s from './styles';
+import StarRatingComponent from 'react-star-rating-component';
 
 import '../../../node_modules/react-image-gallery/styles/css/image-gallery.css';
 import parfume1 from '../../images/parfume1.jpg';
@@ -23,8 +14,20 @@ import 'react-tabs/style/react-tabs.css';
 
 class WardrobePage extends Component {
   state = {
-    rating: 1,
     isTruncated: false,
+    tabOpen: true,
+    rating: 1,
+  };
+
+  onStarClick(nextValue, prevValue, name) {
+    this.setState({ rating: nextValue });
+  }
+
+  toggleTab = e => {
+    const tabValue = e.target.value;
+    this.setState({
+      tabOpen: tabValue,
+    });
   };
 
   toggleTruncate = () => {
@@ -34,77 +37,113 @@ class WardrobePage extends Component {
   };
 
   render() {
-    const { isTruncated } = this.state;
+    const { tabOpen, isTruncated, value, rating } = this.state;
+    const nombre = 1;
     return (
-      <div>
-        <QuizTitle>
+      <Fragment>
+        <s.QuizTitle>
           <h1>Wardrobe</h1>
-        </QuizTitle>
-        <Wrapper>
-          <ImageDiv>
+        </s.QuizTitle>
+        <s.Wrapper>
+          <s.ImageDiv>
             <img src={parfume1} />
-          </ImageDiv>
-
-          <ParfumeDiv>
-            <Tabs>
-              <TabList>
-                <Tab>Parfume name</Tab>
-                <Tab>My rating</Tab>
-              </TabList>
-
-              <TabPanel>
-                <div>
-                  <h2>Parfym 1</h2>
-                  <Truncate
-                    lines={isTruncated ? 0 : 15}
-                    ellipsis={
-                      <span>
-                        ...{' '}
-                        <a onClick={this.toggleTruncate}>
-                          {' '}
-                          Read more
-                        </a>
-                      </span>
-                    }
-                  >
-                    The girls looked like tall, exotic grazing
-                    animals, swaying gracefully and unconsciously with
-                    the movement of the train, their high heels like
-                    polished hooves against the gray metal of the
-                    bright void beyond the chain link. Her cheekbones
-                    flaring scarlet as Wizard’s Castle burned,
-                    forehead drenched with azure when Munich fell to
-                    the Tank War, mouth touched with hot gold as a
-                    gliding cursor struck sparks from the banks of
-                    every computer in the puppet place had been a
-                    subunit of Freeside’s security system. The girls
-                    looked like tall, exotic grazing animals, swaying
-                    gracefully and unconsciously with the surgery, he
-                    {/* <a onClick={this.toggleTruncate}> Read less</a> */}
-                  </Truncate>
-                </div>
-              </TabPanel>
-              <TabPanel>
-                <h2>Rating</h2>
-                <RatingForm onSubmit={this.handleSubmit}>
-                  <label>
-                    My feedback:
-                    <br />
-                    <input
-                      type="textarea"
-                      value={this.state.value}
-                      onChange={this.handleChange}
-                    />
-                  </label>
-                  <input type="submit" value="Submit" />
-                </RatingForm>
-              </TabPanel>
-            </Tabs>
-          </ParfumeDiv>
-        </Wrapper>
-      </div>
+          </s.ImageDiv>
+          <s.ParfumeDiv>
+            <s.ButtonDiv>
+              <button
+                value={'descriptionTab' + nombre}
+                onClick={e => this.toggleTab(e)}
+              >
+                Description
+              </button>
+              <button
+                value={'ratingTab' + nombre}
+                onClick={e => this.toggleTab(e)}
+              >
+                My Rating
+              </button>
+            </s.ButtonDiv>
+            <s.HeaderDiv>Parfym</s.HeaderDiv>
+            <s.StarsDiv>
+              <StarRatingComponent
+                name="rate1"
+                starCount={10}
+                value={rating}
+                onStarClick={this.onStarClick.bind(this)}
+              />
+            </s.StarsDiv>
+            {tabOpen === 'descriptionTab' + nombre ? (
+              <ParfumeWrapper
+                rating={rating}
+                isTruncated={isTruncated}
+                toggleTruncate={this.toggleTruncate}
+              />
+            ) : (
+              <RatingWrapper
+                handleSubmit={this.handleSubmit}
+                handleChange={this.handleChange}
+                value={value}
+                starClick={this.onStarClick}
+              />
+            )}
+          </s.ParfumeDiv>
+        </s.Wrapper>
+      </Fragment>
     );
   }
+}
+
+function ParfumeWrapper({ toggleTruncate, isTruncated }) {
+  return (
+    <Fragment>
+      <s.DescriptionDiv>
+        <Truncate
+          lines={isTruncated ? 0 : 15}
+          ellipsis={
+            <span>
+              ... <a onClick={toggleTruncate}> Read more</a>
+            </span>
+          }
+        >
+          The girls looked like tall, exotic grazing animals, swaying
+          gracefully and unconsciously with the movement of the train,
+          their high heels like polished hooves against the gray metal
+          of the bright void beyond the chain link. Her cheekbones
+          flaring scarlet as Wizard’s Castle burned, forehead drenched
+          with azure when Munich fell to the Tank War, mouth touched
+          with hot gold as a gliding cursor struck sparks from the
+          banks of every computer in the puppet place had been a
+          subunit of Freeside’s security system. The girls looked like
+          tall, exotic grazing animals, swaying gracefully and
+          unconsciously with the surgery,
+          <a onClick={toggleTruncate}> Read less</a>
+        </Truncate>
+      </s.DescriptionDiv>
+    </Fragment>
+  );
+}
+
+function RatingWrapper({ handleSubmit, handleChange, value }) {
+  return (
+    <Fragment>
+      <s.RatingForm onSubmit={handleSubmit}>
+        <label>My feedback:</label>
+
+        <s.RatingBox
+          type="text"
+          className="ratingBox"
+          value={value}
+          onChange={handleChange}
+        />
+
+        <s.RatingButton
+          className="ratingButton"
+          type="submit"
+          value="Submit"
+        />
+      </s.RatingForm>
+    </Fragment>
+  );
 }
 
 const mapStateToProps = state => ({
--- src/components/Wardrobe/styles.js
+++ src/components/Wardrobe/styles.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import styled from 'styled-components';
 
 export const QuizTitle = styled.div`
@@ -12,35 +11,75 @@ export const Wrapper = styled.div`
   width: 100%;
   border: 1px solid black;
   display: flex;
-  justify-content: center;
-  align-content: center;
 `;
 
-export const ParfumeDiv = styled.div`
-  margin: 1em;
-  flex: 1;
-  border: 1px solid green;
-  min-height: 25rem;
-  display: flex;
-`;
-
+// ONE WHOLE CARD
 export const ImageDiv = styled.div`
-  margin: 1em;
+  margin: 0.5em;
   flex: 1;
   border: 1px solid red;
   display: flex;
-  display: flex;
   justify-content: center;
-
   img {
     align-self: center;
+    width: 50%;
+    height: auto;
   }
 `;
 
-export const RatingForm = styled.form`
-  width: 100%;
-  border: 1px solid yellow;
+export const ParfumeDiv = styled.div`
+  margin: 0.5em;
+  flex: 1;
+  border: 1px solid green;
   display: flex;
   flex-direction: column;
-  height: 100%;
+`;
+
+export const StarsDiv = styled.div`
+  flex: 1;
+  border: 1px solid black;
+  font-size: 1.5em;
+`;
+export const HeaderDiv = styled.div`
+  flex: 1;
+  border: 1px solid black;
+`;
+
+//DESCRIPTION TAB
+
+export const DescriptionDiv = styled.div`
+  flex: 8;
+`;
+
+export const ButtonDiv = styled.div`
+  display: flex;
+  flex: 1;
+  /* border: 1px solid black; */
+  flex-direction: row;
+  button {
+    cursor: pointer;
+    background: none;
+    border: 1px solid black;
+  }
+`;
+
+//MY RATING TAB
+
+export const RatingForm = styled.form`
+  border: 1px solid yellow;
+  display: flex;
+  /* align-items: stretch; */
+  flex-direction: column;
+  flex: 8;
+  label {
+    font-size: 1.5em;
+  }
+`;
+
+export const RatingBox = styled.input`
+  flex: 8;
+`;
+
+export const RatingButton = styled.input`
+  flex: 1;
 `;

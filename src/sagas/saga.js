32142d892afae7fdfccfe342e2dc7fd4d229347a
paz198787 <patrick.sjoberg@student.kyh.se>
2019-04-28 12:00:54 +0200

    ok

--- src/sagas/saga.js
+++ src/sagas/saga.js
@@ -9,14 +9,14 @@ function* fetchAllData() {
   yield put({ type: 'FETCH_DATA_SUCCESS', payload: data.data });
 }
 
-function* removeFromDatabase({ id }) {
-  yield call(
-    [axios, axios.delete],
-    `http://localhost:4000/parfumes/${id}`,
-  );
-}
+// function* removeFromDatabase({ id }) {
+//   yield call(
+//     [axios, axios.delete],
+//     `http://localhost:4000/parfumes/${id}`,
+//   );
+// }
 
 export function* watchGetAll() {
   yield takeLatest('GET_ALL_TABLES', fetchAllData);
-  yield takeLatest('REMOVE_ID_FROM_TABLE', removeFromDatabase);
+  // yield takeLatest('REMOVE_ID_FROM_TABLE', removeFromDatabase);
 }

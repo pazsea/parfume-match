2960c033184cc2fedef1d5b4f3e0c2ba31d7f57e
paz198787 <patrick.sjoberg@student.kyh.se>
2019-05-08 09:51:32 +0200

    recommendation page is connected to quiz via fb

--- src/components/Recommendation/Collection.js
+++ src/components/Recommendation/Collection.js
@@ -17,80 +17,89 @@ import { withAuthorization } from '../Session';
 import { withFirebase } from '../Firebase';
 
 class Collection extends Component {
-  state = {};
+  state = {
+    loading: true,
+  };
 
-  async componentWillMount() {
-    this.getCollection();
-  }
-  getCollection() {
+  componentDidMount() {
     this.props.firebase
       .user(this.props.authUser.uid)
       .child('recommendedCol')
       .once('value', snapshot => {
-        const collection = Object.keys(snapshot.val());
-        // console.log(collection);
-        this.setState({ selected: collection });
+        if (snapshot.val()) {
+          const collection = Object.keys(snapshot.val());
+          this.setState({ selectedCol: collection, loading: false });
+        } else {
+          return null;
+        }
       });
   }
 
   render() {
-    const { selected } = this.state;
-    console.log('RENDER ' + selected);
-    // console.log('PROPS ' + this.props.colSuggested);
-    // const {
-    //   image,
-    //   headerImage,
-    //   title,
-    //   firstDescription,
-    // } = this.props[selected];
-    return <div>heeej</div>;
+    const { loading, selectedCol } = this.state;
 
-    //   <Fragment>
-    //     <Header headerImage={headerImage} />
+    if (loading || selectedCol === undefined) {
+      return (
+        <h2>
+          Du har ingen rekommenderad kollektion. Har du fyllt i quiz
+          spelet?
+        </h2>
+      );
+    } else {
+      const {
+        image,
+        headerImage,
+        title,
+        firstDescription,
+      } = this.props[selectedCol];
+      return (
+        <Fragment>
+          <Header headerImage={headerImage} />
 
-    //     <FlexContainer>
-    //       <ImageFlexContainer>
-    //         <img src={image} alt="" />
-    //         <img src={genericParfumeBottle} alt="" />
-    //       </ImageFlexContainer>
-    //       <TextFlexContainer>
-    //         <h1>{title}</h1>
-    //         <h2>159 KR/MÅNAD</h2>
+          <FlexContainer>
+            <ImageFlexContainer>
+              <img src={image} alt="" />
+              <img src={genericParfumeBottle} alt="" />
+            </ImageFlexContainer>
+            <TextFlexContainer>
+              <h1>{title}</h1>
+              <h2>159 KR/MÅNAD</h2>
 
-    //         <SubscribeButton>
-    //           <button>
-    //             <Link to={ROUTES.HOME}>Prenumerera</Link>
-    //           </button>
-    //         </SubscribeButton>
+              <SubscribeButton>
+                <button>
+                  <Link to={ROUTES.HOME}>Prenumerera</Link>
+                </button>
+              </SubscribeButton>
 
-    //         <Description>
-    //           {firstDescription.split('/n').map((desc, index) => (
-    //             <p>{desc}</p>
-    //           ))}
-    //           <ul>
-    //             <li>Ny doft varje månad</li>
-    //             <li>Smart etui att ta med</li>
-    //             <li>Dofter utvalda av experter</li>
-    //             <li>Byt eller pausa när du vill</li>
-    //             <li>Ingen bindningstid</li>
-    //             <li>Fri frakt</li>
-    //           </ul>
-    //           <br />
-    //           <br />
-    //           <p>
-    //             <i>
-    //               <b>Sniph</b> är en prenumerationstjänst som gör det
-    //               enkelt att upptäcka nya dofter. Vi skickar dig varje
-    //               månad mindre flaskor med kända parfymer, som du
-    //               använder i vårt smarta etui. Prenumerationen kostar
-    //               159 kr/mån utan bindningstid.
-    //             </i>
-    //           </p>
-    //         </Description>
-    //       </TextFlexContainer>
-    //     </FlexContainer>
-    //   </Fragment>
-    // );
+              <Description>
+                {firstDescription.split('/n').map((desc, index) => (
+                  <p>{desc}</p>
+                ))}
+                <ul>
+                  <li>Ny doft varje månad</li>
+                  <li>Smart etui att ta med</li>
+                  <li>Dofter utvalda av experter</li>
+                  <li>Byt eller pausa när du vill</li>
+                  <li>Ingen bindningstid</li>
+                  <li>Fri frakt</li>
+                </ul>
+                <br />
+                <br />
+                <p>
+                  <i>
+                    <b>Sniph</b> är en prenumerationstjänst som gör
+                    det enkelt att upptäcka nya dofter. Vi skickar dig
+                    varje månad mindre flaskor med kända parfymer, som
+                    du använder i vårt smarta etui. Prenumerationen
+                    kostar 159 kr/mån utan bindningstid.
+                  </i>
+                </p>
+              </Description>
+            </TextFlexContainer>
+          </FlexContainer>
+        </Fragment>
+      );
+    }
   }
 }
 

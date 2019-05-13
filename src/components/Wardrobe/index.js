5a2b755a9d1f847e86957549afd1aac8110db418
paz198787 <patrick.sjoberg@student.kyh.se>
2019-05-13 18:04:23 +0200

    desc and rating to firebase

--- src/components/Wardrobe/index.js
+++ src/components/Wardrobe/index.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component, Fragment, useState } from 'react';
 import { connect } from 'react-redux';
 import Truncate from 'react-truncate';
 import { compose } from 'recompose';
@@ -13,12 +13,57 @@ import parfume1 from '../../images/parfume1.jpg';
 class WardrobePage extends Component {
   state = {
     isTruncated: false,
-    tabOpen: 'descriptionTab1',
-    rating: 1,
+    tabOpen: '',
+    loading: true,
+    // rating: 5,
   };
 
+  componentDidMount() {
+    const { firebase, currentUser } = this.props;
+    firebase.wardrobe(currentUser).on('value', snapshot => {
+      const myRating = snapshot.val();
+      if (myRating) {
+        this.setState({ loading: false, myRating });
+      } else {
+        this.setState({ loading: false });
+      }
+    });
+  }
+  componentWillUnmount() {
+    const { firebase, currentUser } = this.props;
+    firebase.wardrobe(currentUser).off();
+  }
+
+  descriptionChange(event, name) {
+    const value = event.target.value;
+    console.log(this.state.myRating[name].ownDesc);
+    const path = this.state.myRating[name];
+
+    this.setState({ value });
+
+    // this.setState({ [name + ' ' + 'textEdit']: value });
+  }
+
+  descriptionSubmit(event, name) {
+    // console.log(name);
+    event.preventDefault();
+
+    const text = event.target.value;
+    const { firebase, currentUser } = this.props;
+
+    firebase
+      .wardrobe(currentUser)
+      .child(name)
+      .update({ ownDesc: text });
+  }
+
   onStarClick(nextValue, prevValue, name) {
-    this.setState({ rating: nextValue });
+    const { firebase, currentUser } = this.props;
+
+    firebase
+      .wardrobe(currentUser)
+      .child(name)
+      .update({ rating: nextValue });
   }
 
   toggleTab = e => {
@@ -35,107 +80,138 @@ class WardrobePage extends Component {
   };
 
   render() {
-    const { tabOpen, isTruncated, value, rating } = this.state;
+    const { tabOpen, isTruncated, loading } = this.state;
     const { collection } = this.props;
-    const nombre = 1;
-    return (
-      <Section>
-        <s.QuizTitle>
-          <h1>Wardrobe</h1>
-        </s.QuizTitle>
-        {collection.slice(0, 3).map(item => (
-          <Fragment>
-            <s.Wrapper>
-              <s.ImageDiv>
-                <img alt="parfume bottle" src={parfume1} />
-              </s.ImageDiv>
-              <s.ParfumeDiv>
-                <s.ButtonDiv tabOpen={tabOpen}>
-                  <button
-                    value={'descriptionTab' + nombre}
-                    onClick={e => this.toggleTab(e)}
-                  >
-                    Description
-                  </button>
-                  <button
-                    value={'ratingTab' + nombre}
-                    onClick={e => this.toggleTab(e)}
-                  >
-                    My Rating
-                  </button>
-                </s.ButtonDiv>
-                <s.HeaderDiv>{item.name}</s.HeaderDiv>
-                <s.StarsDiv>
-                  <StarRatingComponent
-                    name="rate1"
-                    starCount={5}
-                    value={rating}
-                    onStarClick={this.onStarClick.bind(this)}
-                  />
-                </s.StarsDiv>
-                {tabOpen === 'descriptionTab' + nombre ||
-                tabOpen === '' ? (
-                  <DescriptionWrapper
-                    rating={rating}
-                    isTruncated={isTruncated}
-                    toggleTruncate={this.toggleTruncate}
-                    tabOpen={tabOpen}
-                  />
-                ) : tabOpen === 'ratingTab' + nombre ? (
-                  <RatingWrapper
-                    handleSubmit={this.handleSubmit}
-                    handleChange={this.handleChange}
-                    value={value}
-                    starClick={this.onStarClick}
-                    tabOpen={tabOpen}
-                  />
-                ) : null}
-              </s.ParfumeDiv>
-            </s.Wrapper>
-          </Fragment>
-        ))}
-      </Section>
-    );
+    if (loading) {
+      return <p>Loading...</p>;
+    } else {
+      return (
+        <Section>
+          <s.QuizTitle>
+            <h1>Wardrobe</h1>
+          </s.QuizTitle>
+          {collection.slice(0, 2).map((item, index) => (
+            <Fragment>
+              <s.Wrapper>
+                <s.ImageDiv>
+                  <img alt="parfume bottle" src={parfume1} />
+                </s.ImageDiv>
+                <s.ParfumeDiv>
+                  <s.ButtonDiv tabOpen={tabOpen}>
+                    <button
+                      value={'descriptionTab' + index}
+                      onClick={e => this.toggleTab(e)}
+                    >
+                      Description
+                    </button>
+                    <button
+                      value={'ratingTab' + index}
+                      onClick={e => this.toggleTab(e)}
+                    >
+                      My Rating
+                    </button>
+                  </s.ButtonDiv>
+                  <s.HeaderDiv>{item.name}</s.HeaderDiv>
+                  <s.StarsDiv>
+                    <StarRatingComponent
+                      key={item.name + index}
+                      name={item.name}
+                      starCount={5}
+                      value={
+                        this.state.myRating &&
+                        this.state.myRating[item.name] &&
+                        this.state.myRating[item.name].rating
+                          ? this.state.myRating[item.name].rating
+                          : 0
+                      }
+                      onStarClick={this.onStarClick.bind(this)}
+                    />
+                  </s.StarsDiv>
+
+                  {tabOpen === 'ratingTab' + index ? (
+                    <RatingWrapper
+                      name={item.name}
+                      firebase={this.props.firebase}
+                      currentUser={this.props.currentUser}
+                      textFirebase={
+                        this.state.myRating[item.name].ownDesc
+                          ? this.state.myRating[item.name].ownDesc
+                          : ''
+                      }
+                      tabOpen={tabOpen}
+                    />
+                  ) : (
+                    <DescriptionWrapper
+                      isTruncated={isTruncated}
+                      toggleTruncate={this.toggleTruncate}
+                      tabOpen={tabOpen}
+                    />
+                  )}
+                </s.ParfumeDiv>
+              </s.Wrapper>
+            </Fragment>
+          ))}
+        </Section>
+      );
+    }
   }
 }
 
 function DescriptionWrapper({ toggleTruncate, isTruncated }) {
   return (
-    <Fragment>
-      <s.DescriptionDiv>
-        <Truncate
-          lines={isTruncated ? 0 : 9}
-          ellipsis={
-            <span>
-              ... <a onClick={toggleTruncate}> Read more</a>
-            </span>
-          }
-        >
-          2017 års stora lansering från parfymprofilen Pierre
-          Guillaumes doftkollektion "Huitième Art" är en makalös
-          splash av fruktiga ackord, träiga noter och mustiga kryddor.
-          2017 års stora lansering från parfymprofilen Pierre
-          Guillaumes doftkollektion "Huitième Art" är en makalös
-          splash av fruktiga ackord, träiga noter och mustiga kryddor.
-          <br />
-          Att du inom kort känner att du bara måste duscha dig i
-          Aqaysos är inte otänkbart. Blir detta doften som förändrar
-          ditt liv?
-        </Truncate>
-      </s.DescriptionDiv>
-    </Fragment>
+    <s.DescriptionDiv>
+      <Truncate
+        lines={isTruncated ? 0 : 9}
+        ellipsis={
+          <span>
+            ... <a onClick={toggleTruncate}> Read more</a>
+          </span>
+        }
+      >
+        2017 års stora lansering från parfymprofilen Pierre Guillaumes
+        doftkollektion "Huitième Art" är en makalös splash av fruktiga
+        ackord, träiga noter och mustiga kryddor. 2017 års stora
+        lansering från parfymprofilen Pierre Guillaumes doftkollektion
+        "Huitième Art" är en makalös splash av fruktiga ackord, träiga
+        noter och mustiga kryddor.
+        <br />
+        Att du inom kort känner att du bara måste duscha dig i Aqaysos
+        är inte otänkbart. Blir detta doften som förändrar ditt liv?
+      </Truncate>
+    </s.DescriptionDiv>
   );
 }
 
-function RatingWrapper({ handleSubmit, handleChange, value }) {
+function RatingWrapper({
+  name,
+  textFirebase,
+  firebase,
+  currentUser,
+}) {
+  const [editText, setEditText] = useState(textFirebase);
+
+  const textChange = e => {
+    const text = e.target.value;
+    setEditText(text);
+  };
+
+  const descriptionSubmit = event => {
+    event.preventDefault();
+
+    firebase
+      .wardrobe(currentUser)
+      .child(name)
+      .update({ ownDesc: editText });
+  };
+
   return (
     <Fragment>
-      <s.RatingForm onSubmit={handleSubmit}>
+      <s.RatingForm onSubmit={e => descriptionSubmit(e)}>
         <s.RatingBox
-          type="textarea"
+          type="text"
           className="ratingBox"
-          value={value}
-          onChange={handleChange}
+          value={editText}
+          onChange={e => textChange(e)}
         />
 
         <s.RatingButton
@@ -149,7 +225,7 @@ function RatingWrapper({ handleSubmit, handleChange, value }) {
 }
 
 const mapStateToProps = state => ({
-  users: state.userState.users,
+  currentUser: state.sessionState.authUser.uid,
   collection: state.sortedParfumesState.Clean,
 });
 

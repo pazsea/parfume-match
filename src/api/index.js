df13d964467a70c15118fcbf41a60493d8be37c8
paz198787 <patrick.sjoberg@student.kyh.se>
2019-05-25 20:20:59 +0200

    api changes, heroku live

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,5 @@
-export const urlGet = 'http://localhost:4000/parfumes/';
-export const urlAdd = 'http://localhost:4000/parfumes/add/';
-export const urlDel = 'http://localhost:4000/parfumes/';
-export const urlUpd = 'http://localhost:4000/parfumes/';
+export const urlGet = 'https://parfume-server.herokuapp.com/parfumes';
+export const urlAdd =
+  'https://parfume-server.herokuapp.com/parfumes/add/';
+export const urlDel = 'https://parfume-server.herokuapp.com/parfumes';
+export const urlUpd = 'https://parfume-server.herokuapp.com/parfumes';
